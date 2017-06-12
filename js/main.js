724285efa336d1272dfdbb12d55e8c5060b45a1f
NarayanChakraborty <narayanchakraborty1993@gmail.com>
2017-06-12 16:28:09 +0600

    Menu , Slider and Service Section Completed

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    
+    // Isotope initialization
+        var $item = $('.item').isotope({
+          itemSelector: '.item-box',
+        });
+    // filter items on button click
+    $('.filter-button-group').on( 'click', 'a.btnf', function() {
+      var filterValue = $(this).attr('data-filter');
+      $item.isotope({ filter: filterValue });
+
+    });
+    
+});
