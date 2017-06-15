6aa75e3ed4081a60a559e218d26b9922dc59249f
NarayanChakraborty <narayanchakraborty1993@gmail.com>
2017-06-15 08:46:10 +0600

    Client Feedback Section Done

--- js/main.js
+++ js/main.js
@@ -16,4 +16,24 @@ $(document).ready(function () {
                 time: 1000
             });
     
+    $('.owl-carousel').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true,
+            navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1,
+
+                },
+                600: {
+                    items: 1,
+                },
+                1000: {
+                    items: 1,
+                    loop: true
+                }
+            }
+        });
 });
